b7e08ffc7f7ae88edc6b400cf1ab5fbe3f97f14c
iam7kei <iam7kei@gmail.com>
2022-10-25 01:58:31 +0800

    auth commit

---
+++ models/RegisterModel.php
@@ -0,0 +1,11 @@
+<?php
+
+class RegisterModel
+{
+    public string $name;
+    public string $gender;
+    public string $address;
+    public string $dob;
+    public string $password;
+    public string $confirmPassword;
+}
